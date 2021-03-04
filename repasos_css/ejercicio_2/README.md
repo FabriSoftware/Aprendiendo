96865fc981312ad0f3e158989b179f8ca526c5e5
Fabriziodeveloper <fabrizioplaza87@gmail.com>
2021-03-04 15:27:00 -0300

    ejercicio 2 (css)

---
+++ repasos_css/ejercicio_2/README.md
@@ -0,0 +1,5 @@
+## ejercicio N°1
+
+captura de la pagina web
+
+![alt text](https://i.imgur.com/h6D6fIC.png)
---
+++ repasos_css/ejercicio_2/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>ejercicio N°2</title>
+</head>
+
+<body>
+    <div class="container">
+        <img align="right" src="https://placehold.it/150x150/FFFFFF/000000/" alt="Foto de 'tu nombre'">
+        <h1>tu nombre</h1>
+        <h3>datos personales</h3>
+
+        <ul>
+            <li>Lorem ipsum dolor</li>
+            <li>Lorem ipsum dolor</li>
+            <li>Lorem ipsum dolor</li>
+            <li>Lorem ipsum dolor</li>
+        </ul>
+        <h3>informacion personal</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sunt, est quis impedit, aut nemo quasi quos
+            debitis aliquid nisi perferendis commodi recusandae! Debitis consequatur recusandae provident similique quae
+            magni.</p>
+     <form action="">
+         <input type="text" placeholder="Your Name" required />
+         <input type="email" placeholder="Your email" required />
+         <textarea cols="30" rows="10" placeholder="Your Message" required></textarea>
+         <input type="submit" value="submit">
+     </form>
+    </div>
+</body>
+
+</html>
