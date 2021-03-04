0f7a8c2671eb75df1c77a96aa29e088ea81af7d1
Fabriziodeveloper <fabrizioplaza87@gmail.com>
2021-03-04 11:13:28 -0300

    ejercicio 1 (css)

---
+++ repasos_css/ejercicio_1/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Proyecto N°1</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>CSS pruebas</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#">pagina 1</a></li>
+                <li><a href="#">pagina 2</a></li>
+                <li><a href="#">pagina 3</a></li>
+                <li><a href="#">pagina 4</a></li>
+                <li><a href="#">pagina 5</a></li>
+            </ul>
+        </nav>
+        <div class="clearfix"></div>
+        <section class="content">
+            <article class="article">
+                <h2>article title</h2>
+                <p>article text</p>
+            </article>
+            <article class="article">
+                <h2>article title</h2>
+                <p>article text</p>
+            </article>
+            <article class="article">
+                <h2>article title</h2>
+                <p>article text</p>
+            </article>
+            <article class="article">
+                <h2>article title</h2>
+                <p>article text</p>
+            </article>
+        </section>
+
+        <aside>
+            <h2>lateral bar</h2>
+            <form action="#" method="post">
+                <input type="text">
+                <input type="submit" value="buscar">
+            </form>
+        </aside>
+        <div class="clearfix"></div>
+        <footer>
+            &#xA9; serverpide
+        </footer>
+    </div>
+</body>
+
+</html>
